b812ae2471c3cbd1dfab851faf12cba5c9c9ae77
Adithyan <adithyan92014@gmail.com>
2020-09-29 10:47:49 +0530

    added video and change some texts

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+// import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+    <div className="hero-container">
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>Experience Kerala</h1>
+      <p>Take a peek at what God’s Own Country has to offer you!</p>
+      <div className="hero-btns">
+        {/* <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           GET STARTED
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button> */}
       </div>
     </div>
   );
